13ee7494b0d725c433b8779eb66f3699596c493b
kdikiller <1450844869@qq.com>
2020-12-11 17:57:51 +0800

    upload

---
+++ src/pages/home/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    name:"index",
+    data(){
+        return {
+
+        };
+    },
+    created(){
+
+    },
+    methods:{
+        
+    }
+}
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+@import url("./index.css");
+</style>
